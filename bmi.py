4a00aed2eff0dd6d3dc3fe128287ef79854fe55d
Erik Ruzek <bbachilles@gmail.com>
2015-04-17 14:05:56 -0400

    Add weight and height units

--- bmi.py
+++ bmi.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-w = raw_input("enter weight: ")
-h = raw_input("enter height: ")
+w = raw_input("enter weight in lbs: ")
+h = raw_input("enter height in in: ")
 bmi = (float(w)/float(h)**2)*703
 print bmi
